13eca32a012fa21d2eeb49d3cf23216f3a6a4c10
seosungjoon <seojj95@naver.com>
2018-03-15 00:40:41 +0900

    [수정,추가]
    -시작화면 2초로 수정
    -주석 상세표기
    -3번화면 스피너 "선택" 항목 삭제
    -맵뷰에서 3번화면 전환버튼 추가
    -맵뷰 크기조정

--- app/src/main/java/org/androidtown/centerpoint/MapViewPage.java
+++ app/src/main/java/org/androidtown/centerpoint/MapViewPage.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -29,12 +30,13 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.OnConnectionFailedListener{
+public class MapViewPage extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
     private static final String TAG = "MapViewPage";
     private static final LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
+
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2002;
     private GoogleMap googleMap = null;
     private MapView mapView = null;
@@ -42,11 +44,13 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
     private Marker currentMarker = null;
 
     public void setCurrentLocation(Location location, String markerTitle, String markerSnippet) {
-
-        if ( currentMarker != null ) currentMarker.remove();
-        if ( location != null) {
+        //장소를 검색했을때, 해당 장소에 마커를 찍음
+        if (currentMarker != null) currentMarker.remove();
+        Log.i(TAG, "Location :" + location);
+        if (location != null) {
+            Log.d(TAG, "setCurrentLocation: ");
             //현재위치의 위도 경도 가져옴
-            LatLng currentLocation = new LatLng( location.getLatitude(), location.getLongitude());
+            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
             MarkerOptions markerOptions = new MarkerOptions();
             markerOptions.position(currentLocation);
             markerOptions.title(markerTitle);
@@ -57,6 +61,7 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
             this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
             return;
         }
+        //registerLocationUpdates();
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(DEFAULT_LOCATION);
         markerOptions.title(markerTitle);
@@ -69,7 +74,9 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         //맵뷰
+        Log.i(TAG,"testfirst");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_view_page);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -113,18 +120,16 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-    // OnMapReadyCallback implements 해야 mapView.getMapAsync(this); 사용가능. this 가 OnMapReadyCallback
-
+        // OnMapReadyCallback implements 해야 mapView.getMapAsync(this); 사용가능. this 가 OnMapReadyCallback
 
 
         this.googleMap = googleMap;
 
 
-
         //런타임 퍼미션 요청 대화상자나 GPS 활성 요청 대화상자 보이기전에 지도의 초기위치를 서울로 이동
+        Log.i(TAG, "start");
 
         setCurrentLocation(null, "위치정보 가져올 수 없음", "위치 퍼미션과 GPS 활성 여부 확인");
-
 
 
         //나침반이 나타나도록 설정
@@ -136,7 +141,6 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
 
 
-
         //  API 23 이상이면 런타임 퍼미션 처리 필요
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -146,8 +150,7 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
             int hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
 
 
-
-            if ( hasFineLocationPermission == PackageManager.PERMISSION_DENIED) {
+            if (hasFineLocationPermission == PackageManager.PERMISSION_DENIED) {
 
                 //사용권한이 없을경우
 
@@ -159,14 +162,13 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
 
                 //사용권한이 있는경우
 
-                if ( googleApiClient == null) {
+                if (googleApiClient == null) {
                     ;
 
                 }
 
 
-
-                if ( ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
 
                 {
 
@@ -179,13 +181,10 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
         } else {
 
 
-
-            if ( googleApiClient == null) {
-
+            if (googleApiClient == null) {
 
 
             }
-
 
 
             googleMap.setMyLocationEnabled(true);
@@ -193,13 +192,56 @@ public class MapViewPage extends FragmentActivity implements OnMapReadyCallback,
         }
 
 
+    }
+    public void onButtonClicked(View v) {
+        //맵뷰 화면에서 하단 버튼 클릭 시, 만약 위치값이 null이거나
+        //Default Location일경우
+        //에러와 함께 버튼이 눌리지 않게 해야함
 
-
-
-
+        //좌표값을 3번화면으로 동시에 넘긴다.
+        this.finish();//맵뷰 화면을 종료하고 이전 화면으로 넘어간다.
     }
 
 
+//    LocationManager mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//    private void registerLocationUpdates() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        mLM.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, defaultLocationListener);
+//        mLM.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,1,defaultLocationListener);
+//    }
+//    private final LocationListener defaultLocationListener = new LocationListener() {
+//        @Override
+//        public void onLocationChanged(Location location) {
+//            double longitude = location.getLongitude();//경
+//            double latitude =location.getLatitude();//위
+//            DEFAULT_LOCATION = new LatLng(longitude,latitude);
+//            String provider = location.getProvider();//위치제공자
+//        }
+//
+//        @Override
+//        public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//        }
+//
+//        @Override
+//        public void onProviderEnabled(String provider) {
+//
+//        }
+//
+//        @Override
+//        public void onProviderDisabled(String provider) {
+//
+//        }
+//    };
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
